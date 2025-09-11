2e84505097b5ab009de640f9af5056808f577d00
Larry Peng <peng.larry@outlook.com>
2025-09-10 22:12:40 -0400

    Added code for a demo shooter and intake.

--- java/MecanumTeleOp.java
+++ java/MecanumTeleOp.java
@@ -3,11 +3,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class MecanumTeleOp extends LinearOpMode {
+    private DcMotor frontLeftMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backRightMotor;
+    private DcMotor shootMotor;
+    private CRServo intakeServo;
+
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
@@ -16,6 +24,12 @@ public class MecanumTeleOp extends LinearOpMode {
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        shootMotor = hardwareMap.dcMotor.get("shootMotor");
+        intakeServo = hardwareMap.crservo.get("intakeServo");
 
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
@@ -26,11 +40,17 @@ public class MecanumTeleOp extends LinearOpMode {
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        // You may need to reverse the shooter motor or intake servo depending on how they are mounted.
+        // For example:
+        // shootMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        // intakeServo.setDirection(DcMotorSimple.Direction.REVERSE);
+
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
+            // --- Drive Train Control ---
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
@@ -48,6 +68,15 @@ public class MecanumTeleOp extends LinearOpMode {
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+
+            // --- Intake and Shooter Control ---
+            // The triggers return a value from 0.0 to 1.0, which is perfect for continuous power
+            double intakePower = gamepad1.left_trigger;
+            double shootPower = gamepad1.right_trigger;
+
+            // Set power to the intake servo and shooter motor
+            intakeServo.setPower(intakePower);
+            shootMotor.setPower(shootPower);
         }
     }
 }
